1b551ecc39e74dc65bc819a9d8c0bbfc3c48deed
Wilson Fernandez <wfernandez@wind.com.do>
2017-07-06 11:43:39 -0400

    primera carga

---
+++ AbpZero310-master/src/MyCompanyName.AbpZeroTemplate.Web/App_Start/Navigation/PageNames.cs
@@ -0,0 +1,42 @@
+namespace MyCompanyName.AbpZeroTemplate.Web.Navigation
+{
+    public static class PageNames
+    {
+        public static class App
+        {
+            public static class Common
+            {
+                public const string Administration = "Administration";
+                public const string Roles = "Administration.Roles";
+                public const string Users = "Administration.Users";
+                public const string AuditLogs = "Administration.AuditLogs";
+                public const string OrganizationUnits = "Administration.OrganizationUnits";
+                public const string Languages = "Administration.Languages";
+
+                public const string Shop = "Shop";
+                public const string Category = "Category";
+            }
+
+            public static class Host
+            {
+                public const string Tenants = "Tenants";
+                public const string Editions = "Editions";
+                public const string Maintenance = "Administration.Maintenance";
+                public const string Settings = "Administration.Settings.Host";
+            }
+
+            public static class Tenant
+            {
+                public const string Dashboard = "Dashboard.Tenant";
+                public const string Settings = "Administration.Settings.Tenant";
+                public const string Test = "Test";//这里是添加的常量
+            }
+        }
+
+        public static class Frontend
+        {
+            public const string Home = "Frontend.Home";
+            public const string About = "Frontend.About";
+        }
+    }
+}
---
+++ AbpZero310-master/src/MyCompanyName.AbpZeroTemplate.Web/Areas/Mpa/Startup/MpaNavigationProvider.cs
@@ -0,0 +1,125 @@
+﻿using Abp.Application.Navigation;
+using Abp.Localization;
+using MyCompanyName.AbpZeroTemplate.Authorization;
+using MyCompanyName.AbpZeroTemplate.Web.Navigation;
+
+namespace MyCompanyName.AbpZeroTemplate.Web.Areas.Mpa.Startup
+{
+    public class MpaNavigationProvider : NavigationProvider
+    {
+        public const string MenuName = "Mpa";
+        
+        public override void SetNavigation(INavigationProviderContext context)
+        {
+            var menu = context.Manager.Menus[MenuName] = new MenuDefinition(MenuName, new FixedLocalizableString("Main Menu"));
+
+            menu
+                .AddItem(new MenuItemDefinition(
+                    PageNames.App.Host.Tenants,
+                    L("Tenants"),
+                    url: "Mpa/Tenants",
+                    icon: "icon-globe",
+                    requiredPermissionName: AppPermissions.Pages_Tenants
+                    )
+                ).AddItem(new MenuItemDefinition(
+                PageNames.App.Tenant.Test,//一个常量，控制菜单是否被选中
+　　                L("Test"),//菜单显示名称，在语言文件中配置
+　　                url: "Mpa/Test",//菜单路径
+　　                icon: "icon-globe",//菜单图标
+                  requiredPermissionName: AppPermissions.Pages_Administration_Test//菜单权限，登录用户所在角色有此权限才会显示出来
+                )).AddItem(new MenuItemDefinition(
+                    PageNames.App.Host.Editions,
+                    L("Editions"),
+                    url: "Mpa/Editions",
+                    icon: "icon-grid",
+                    requiredPermissionName: AppPermissions.Pages_Editions
+                    )
+                ).AddItem(new MenuItemDefinition(
+                    PageNames.App.Tenant.Dashboard,
+                    L("Dashboard"),
+                    url: "Mpa/Dashboard",
+                    icon: "icon-home",
+                    requiredPermissionName: AppPermissions.Pages_Tenant_Dashboard
+                    )
+                ).AddItem(new MenuItemDefinition(
+                    PageNames.App.Common.Administration,
+                    L("Administration"),
+                    icon: "icon-wrench"
+                    ).AddItem(new MenuItemDefinition(
+                        PageNames.App.Common.OrganizationUnits,
+                        L("OrganizationUnits"),
+                        url: "Mpa/OrganizationUnits",
+                        icon: "icon-layers",
+                        requiredPermissionName: AppPermissions.Pages_Administration_OrganizationUnits
+                        )
+                    ).AddItem(new MenuItemDefinition(
+                        PageNames.App.Common.Roles,
+                        L("Roles"),
+                        url: "Mpa/Roles",
+                        icon: "icon-briefcase",
+                        requiredPermissionName: AppPermissions.Pages_Administration_Roles
+                        )
+                    ).AddItem(new MenuItemDefinition(
+                        PageNames.App.Common.Users,
+                        L("Users"),
+                        url: "Mpa/Users",
+                        icon: "icon-users",
+                        requiredPermissionName: AppPermissions.Pages_Administration_Users
+                        )
+                    ).AddItem(new MenuItemDefinition(
+                        PageNames.App.Common.Languages,
+                        L("Languages"),
+                        url: "Mpa/Languages",
+                        icon: "icon-flag",
+                        requiredPermissionName: AppPermissions.Pages_Administration_Languages
+                        )
+                    ).AddItem(new MenuItemDefinition(
+                        PageNames.App.Common.AuditLogs,
+                        L("AuditLogs"),
+                        url: "Mpa/AuditLogs",
+                        icon: "icon-lock",
+                        requiredPermissionName: AppPermissions.Pages_Administration_AuditLogs
+                        )
+                    ).AddItem(new MenuItemDefinition(
+                        PageNames.App.Host.Maintenance,
+                        L("Maintenance"),
+                        url: "Mpa/Maintenance",
+                        icon: "icon-wrench",
+                        requiredPermissionName: AppPermissions.Pages_Administration_Host_Maintenance
+                        )
+                    )
+                    .AddItem(new MenuItemDefinition(
+                        PageNames.App.Host.Settings,
+                        L("Settings"),
+                        url: "Mpa/HostSettings",
+                        icon: "icon-settings",
+                        requiredPermissionName: AppPermissions.Pages_Administration_Host_Settings
+                        )
+                    ).AddItem(new MenuItemDefinition(
+                        PageNames.App.Tenant.Settings,
+                        L("Settings"),
+                        url: "Mpa/Settings",
+                        icon: "icon-settings",
+                        requiredPermissionName: AppPermissions.Pages_Administration_Tenant_Settings
+                        )
+                    )
+                ).AddItem(new MenuItemDefinition(
+                    PageNames.App.Common.Shop,//一个常量，控制菜单是否被选中
+                    L("Shop"),//菜单显示名称，在语言文件中配置
+                    icon: "icon-globe"//菜单图标
+                    ).AddItem(new MenuItemDefinition(
+                        //子菜单
+                        PageNames.App.Common.Category,
+                        L("CategoryManager"),
+                        url: "Mpa/Category",
+                        icon: "icon-globe"
+                        ))
+                );
+        }
+
+        private static ILocalizableString L(string name)
+        {
+            return new LocalizableString(name, AbpZeroTemplateConsts.LocalizationSourceName);
+        }
+    }
+}
